2787def408ea7715d948d455523b56ffd1e89243
jgilot <ju.gilot@outlook.com>
2022-05-19 14:52:09 +0200

    Ajout du système de pagination

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,11 +5,13 @@ namespace App\Controller;
 use App\Entity\Article;
 use App\Form\NewArticleFormType;
 use Doctrine\Persistence\ManagerRegistry;
+use Knp\Component\Pager\PaginatorInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
@@ -80,12 +82,26 @@ class BlogController extends AbstractController
      * Controleur de la page qui liste les articles
      */
     #[Route('/publication/liste/', name: 'publication_list')]
-    public function publicationList(ManagerRegistry $doctrine): Response
+    public function publicationList(ManagerRegistry $doctrine, Request $request, PaginatorInterface $paginator): Response
     {
 
-        $articleRepo = $doctrine->getRepository(Article::class);
+        // Récupération de $_GET['page'], 1 si elle n'existe pas
+        $requestedPage = $request->query->getInt('page', 1);
 
-        $articles = $articleRepo->findAll();
+        if ($requestedPage < 1){
+            throw new NotFoundHttpException();
+        }
+
+        $em = $doctrine->getManager();
+
+        $query = $em->createQuery('SELECT a from App\Entity\Article a ORDER BY a.publicationDate DESC');
+
+        $articles = $paginator->paginate(
+            $query, //Requête créée juste avant
+            $requestedPage,  // Page qu'on souhaite voir
+            10,  // Nomber d'article à afficher par page
+        );
+
 
         return $this->render('blog/publication_list.html.twig', [
             'articles' => $articles,
