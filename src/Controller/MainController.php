950e7da5f9ef744ac2a31f7cf2d8d946822b1a39
jgilot <ju.gilot@outlook.com>
2022-05-19 10:00:14 +0200

    Ajout des derniers articles sur l'accueil

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use Doctrine\Persistence\ManagerRegistry;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bridge\Twig\TokenParser\DumpTokenParser;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,9 +19,22 @@ class MainController extends AbstractController
      */
 
     #[Route('/', name: 'home')]
-    public function home(): Response
+    public function home(ManagerRegistry $doctrine): Response
     {
-        return $this->render('main/home.html.twig');
+
+        $articleRepo = $doctrine->getRepository(Article::class);
+
+        $articles = $articleRepo->findBy(
+            [],  // WHERE du SELECT
+            ['publicationDate' => 'DESC'],  // ORDER BY du SELECT
+            $this->getParameter('app.article.last_article_number_on_home') // LIMIT du SELECT
+        );
+
+        dump( $articles );
+
+        return $this->render('main/home.html.twig', [
+            'articles' => $articles,
+        ]);
     }
 
     #[Route('/mon-profil/', name: 'profil')]
